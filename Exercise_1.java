661a9d787c09d0fdfdd1404919d04632c4531a93
Pranav Sorte <pranavsorte@Pranavs-MacBook-Air.local>
2025-11-01 20:48:02 -0700

    complete Exercise_1

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,30 +7,50 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
-        return true;
+        if(top == -1) {
+            return true;
+        } else {
+            return false;
+        }
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top == MAX-1) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        top++;
+        a[top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()) {
+            System.out.println("System Underflow");
+            return 0;
+        }
+
+        int res = a[top];
+        top--;
+        return res;
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
